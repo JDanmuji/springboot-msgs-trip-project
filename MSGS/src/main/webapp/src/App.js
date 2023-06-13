92a2d3cc27574451cc6feb94d083f4f82d9423f0
eunyoung22 <ey0928337@naver.com>
2023-06-13 17:02:39 +0900

    feature: add FrontEnd Login,LogoutModal,Mypage

--- MSGS/src/main/webapp/src/App.js
+++ MSGS/src/main/webapp/src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/main/Main";
 import Header from "./components/header/Header";
-
+import LoginMain from "./pages/login/LoginMain";
+import MyPageMain from "./pages/mypage/MyPageMain";
+import LogoutModal from "./components/logout/LogoutModal";
 
 const App = () => {
     return (
@@ -11,6 +13,9 @@ const App = () => {
 
             <Routes>
                 <Route path="/" element={<Main />} />
+                <Route path="/login" element={<LoginMain />} />
+                <Route path="/mypage" element={<MyPageMain />} />
+                <Route path="/logout" element={<LogoutModal />} />
             </Routes>
         </BrowserRouter>
     );
---
+++ MSGS/src/main/webapp/src/components/login/LoginByEmail.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const LoginByEmail = () => {
+    return (
+        <div>
+            <div className="email-login-button">
+                <button className="email-button">
+                    <img className="email-icon" src="public/images/email-icon.png"></img> 이메일로 로그인하기
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default LoginByEmail;
