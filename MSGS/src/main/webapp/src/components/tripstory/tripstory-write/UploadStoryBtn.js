dc9b5f197eff742ce9352e57a5642c1f1641d0eb
jihyeon <wisedew2@gmail.com>
2023-06-13 16:44:09 +0900

    FEATURE : Add tripstory function
    Add tripstory function

---
+++ MSGS/src/main/webapp/src/components/tripstory/tripstory-write/UploadStoryBtn.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './UploadStoryBtn.module.css';
+
+// 글쓰기 모달 창의 하단 '완료' 버튼입니다.
+const UploadStoryBtn = ({onClose}) => {
+    return (
+        <div className={styles['upload-story-btn']} onClick={ onClose }>
+            <a href='#'>완료</a>
+        </div>
+    );
+};
+
+export default UploadStoryBtn;
