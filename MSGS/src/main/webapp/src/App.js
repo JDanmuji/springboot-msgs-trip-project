527db93d6f7f08bc3bc997e842e8c25b4dc9017a
6dreamhigh <6dreamhigh@naver.com>
2023-06-13 16:39:26 +0900

    feat:front 여행일정1, 여행일정2 작업 완료

--- MSGS/src/main/webapp/src/App.js
+++ MSGS/src/main/webapp/src/App.js
@@ -1,16 +1,18 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Main from "./pages/main/Main";
-import Header from "./components/header/Header";
-
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './pages/main/Main';
+import Header from './components/header/Header';
+import TripSchedule1 from './pages/tripschedule/tripschedule-details/tipschedule1/TripSchedule1';
+import TripSchedule2 from './pages/tripschedule/tripschedule2/TripSchedule2';
 
 const App = () => {
     return (
         <BrowserRouter>
             <Header />
-
             <Routes>
                 <Route path="/" element={<Main />} />
+                <Route path="/tripschedule1" element={<TripSchedule1 />} />
+                <Route path="/tripschedule2" element={<TripSchedule2 />} />
             </Routes>
         </BrowserRouter>
     );
