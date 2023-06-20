ce2434b087f3318c580e4af12843b071501a79e0
6dreamhigh <6dreamhigh@naver.com>
2023-06-20 10:20:18 +0900

    feat:항공사 api가져와 클릭 옵션에 따라 출력 작업

--- MSGS/src/main/webapp/src/App.js
+++ MSGS/src/main/webapp/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider} from 'react-query'
+
 import TripStoryList from "./pages/tripstory/tripstory-list/TripStoryList";
 import TripScheduleAddModal from "./components/tripschedule/modal/TripScheduleAddModal";
 import Flight from "./pages/flight/Flight";
@@ -33,62 +35,51 @@ import TripStoryDetail from "./pages/tripstory/tripstory-detail/TripStoryDetail"
 import FlightWithData from "./pages/flight/flight-list/FlightWithData";
 
 
+const queryClient = new QueryClient()
 
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="/tripLoc" element={<TripLocDetail />} />
-                <Route path="/tripstory" element={<TripStoryList />} />
-                <Route
-                    path="/TripScheduleAddModal"
-                    element={<TripScheduleAddModal />}
-                />
-                <Route path="/flight" element={<Flight />} />
-                <Route path="/FlightWithData" element={<FlightWithData/>}/>
-                <Route path="/tripSchedule" element={<TripSchedule />} />
-                <Route path="/login" element={<LoginMain />} />
-                <Route path="/naver" element={<LoginSocial />} />
-                <Route path="/mypage" element={<MyPageMain />} />
-                <Route path="/logout" element={<LogoutModal />} />
-                <Route path="/signup1" element={<Signup1 />} />
-                <Route path="/SignupAgreement" element={<SignupAgreement />} />
 
-                <Route path="/SignupFindID" element={<SignupFindID/>}/>
-                <Route path="/AuthenticationNumber" element={<AuthenticationNumber/>}/>
-                <Route path="/tripschedule1" element={<TripSchedule1 />} />
-                <Route path="/tripschedule2" element={<TripSchedule2 />} />
-                <Route path="/tripstory/create" element={<TripStoryCreate />} />
+			<QueryClientProvider client={queryClient}>
+				<BrowserRouter>
+					<Header />
+					<Routes>
+						<Route path='/' element={<Main />} />
+						<Route path='/tripLoc' element={<TripLocDetail />} />
+						<Route path='/tripstory' element={<TripStoryList />} />
+						<Route path='/TripScheduleAddModal' element={<TripScheduleAddModal />} />
+                        <Route path='/flight' element={<Flight />} />
+                        <Route path="/FlightWithData" element={<FlightWithData/>}/>
+						<Route path='/tripSchedule' element={<TripSchedule />} />
+						<Route path='/login' element={<LoginMain />} />
+						<Route path='/naver' element={<LoginSocial />} />
+						<Route path='/mypage' element={<MyPageMain />} />
+						<Route path='/logout' element={<LogoutModal />} />
+						<Route path='/signup1' element={<Signup1 />} />
+						<Route path='/SignupAgreement' element={<SignupAgreement />} />
+
+						<Route path='/SignupFindID' element={<SignupFindID />} />
+						<Route path='/AuthenticationNumber' element={<AuthenticationNumber />} />
+						<Route path='/tripschedule1' element={<TripSchedule1 />} />
+						<Route path='/tripschedule2' element={<TripSchedule2 />} />
+						<Route path='/tripstory/create' element={<TripStoryCreate/>} />
+
+						<Route path='/nickname' element={<NickName />} />
+						<Route path='/noneMemberResSearch' element={<NonMemberResSearch />} />
+
+						<Route path='/ReviewImgModal' element={<ReviewImgModal />} />
+
+                        <Route path='/mypage/profileUpdate' element={<ProfileUpdate />} />
+                        <Route path="/signup/registerPhone" element={<RegisterPhone />} />
+                        <Route path="/tripstory/detail" element={<TripStoryDetail />} />
                 
-                <Route path="/nickname" element={<NickName />} /> 
+					</Routes>
 
-                <Route path="/ReviewImgModal" element={<ReviewImgModal />} />
-                
-                <Route
-                    path="/noneMemberResSearch"
-                    element={<NonMemberResSearch />}
-                />
-                <Route
-                    path="/mypage/profileUpdate"
-                    element={<ProfileUpdate />}
-                />
-                <Route
-                    path="/signup/registerPhone"
-                    element={<RegisterPhone />}
-                />
-
-                
-
-                <Route path="/tripstory/detail" element={<TripStoryDetail />} />
-
-            </Routes>
-
-            <Footer />
-        </BrowserRouter>
-    );
+					<Footer />
+				</BrowserRouter>
+			</QueryClientProvider>
+		)
 };
 
 export default App;
--- MSGS/src/main/webapp/src/pages/tripschedule/TripSchedule.js
+++ MSGS/src/main/webapp/src/pages/tripschedule/TripSchedule.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { Scrollbars } from 'react-custom-scrollbars'
 
-import style from "./TripSchedule.module.css";
+import style from './TripSchedule.module.css'
 
-import DayPlan from "../../components/tripschedule/DayPlan";
-import DayPlanEditMode from "../../components/tripschedule/DayPlanEditMode";
-import SelectedPlace from "../../components/tripschedule/SelectedPlace";
-import Map from "../../components/tripschedule/Map";
+import DayPlan from '../../components/tripschedule/DayPlan'
+import DayPlanEditMode from '../../components/tripschedule/DayPlanEditMode'
+import SelectedPlace from '../../components/tripschedule/SelectedPlace'
+import Map from '../../components/tripschedule/Map'
 
 // export default function TripSchedule({ dateList}) {    <-전 페이지에서 dateList 받아오면.
 export default function TripSchedule() {
@@ -18,177 +18,167 @@ export default function TripSchedule() {
 
   const dateList = [ '2023.6.22', '2023.6.23', '2023.6.24' ]
 
-  //일정 블록들을 저장한 배열
-  const [ planList, planListHandler ] = useState([
-    { order: 1, placeOrder: 1, isChecked: false, type: 'place', title: '경포 해변1', subtitle: '관광명소 · 강릉' },
-    { order: 2, placeOrder: 2, isChecked: false, type: 'place', title: '경포 해변2', subtitle: '관광명소 · 강릉' },
-    { order: 3, placeOrder: null, isChecked: false, type: 'dorm', title: '조선 웨스턴 호텔', subtitle: '숙소 · 강릉' },
-    { order: 4, placeOrder: null, isChecked: false, type: 'memo', title: '중간에 야시장 갈 수 있음', subtitle: null },
-    {
-      order: 5,
-      placeOrder: 3,
-      isChecked: false,
-      type: 'place',
-      title: '에디슨 과학 박물관 & 참소리 축음기',
-      subtitle: '관광명소 · 강릉',
-    },
-  ])
-  
-  const [ scrollPosition, setScrollPosition ] = useState(0) // 현재 스크롤 위치
-  const containerRef = React.useRef(null) // React 컴포넌트에서 DOM 요소에 접근하기 위해 사용
+	//일정 블록들을 저장한 배열
+	const [planList, planListHandler] = useState([
+		{ order: 1, placeOrder: 1, isChecked: false, type: 'place', title: '경포 해변1', subtitle: '관광명소 · 강릉' },
+		{ order: 2, placeOrder: 2, isChecked: false, type: 'place', title: '경포 해변2', subtitle: '관광명소 · 강릉' },
+		{ order: 3, placeOrder: null, isChecked: false, type: 'dorm', title: '조선 웨스턴 호텔', subtitle: '숙소 · 강릉' },
+		{ order: 4, placeOrder: null, isChecked: false, type: 'memo', title: '중간에 야시장 갈 수 있음', subtitle: null },
+		{
+			order: 5,
+			placeOrder: 3,
+			isChecked: false,
+			type: 'place',
+			title: '에디슨 과학 박물관 ',
+			subtitle: '관광명소 · 강릉',
+		},
+		{
+			order: 6,
+			placeOrder: 4,
+			isChecked: false,
+			type: 'place',
+			title: '참소리 축음기',
+			subtitle: '관광명소 · 강릉',
+		},
+	])
 
-  const [ showPrevButton, setShowPrevButton ] = useState(false) // 이동 버튼 show state
-  const [ showNextButton, setShowNextButton ] = useState(true)
+	const [scrollPosition, setScrollPosition] = useState(0) // 현재 스크롤 위치
+	const containerRef = useRef(null) // React 컴포넌트에서 DOM 요소에 접근하기 위해 사용
 
-  /*
+	const [showPrevButton, setShowPrevButton] = useState(false) // slide 왼쪽 이동 버튼 show 
+	const [showNextButton, setShowNextButton] = useState(false) // slide 오른쪽 이동 버튼 show
+
+  //장소, 숙소 
   useEffect(() => {
-    // 마운트 시, 이벤트 등록
-    const container = containerRef.current // 현재 containerRef가 참조하는 DOM 요소를 가리킴
-    if (container) {
-      container.addEventListener('scroll', handleScroll)
-      handleScroll()
+    if (planList.filter(item => item.type !== 'memo').length > 4) { 
+      setShowNextButton(true)
     }
-    return () => {
-      // cleanup
-      if (container) {
-        container.removeEventListener('scroll', handleScroll)
-      }
-    }
-  }, []) // component 첫 rendering 시 1회만 실행
+  }, [ planList ]);
+  
 
-  const handleScroll = () => {
-    const container = containerRef.current
-    if (container) {
-      setShowPrevButton(container.scrollLeft > 0) // container의 가로 스크롤 위치가 0보다 클 때
-      setShowNextButton(
-        container.scrollLeft < container.scrollWidth - container.clientWidth
-        // 오른쪽으로 스크롤할 공간이 남아있다는 것을 의미
-        // scrollWidth: 컨테이너 전체의 가로 크기
-        // clientWidth: 해당 요소의 내용이 보여지는 영역의 가로 크기
-      )
-    }
-  }
-  */
-  const prevBtnHandler = () => {
-    const container = containerRef.current
-    if (container) {
-      const scrollOffset = 325 // 클릭 당 스크롤 이동 범위
-      container.scrollTo({
-        // 컨테이너 스크롤
-        left: scrollPosition - scrollOffset, // 현재 위치 - 클릭 당 스크롤 이동 범위
-        behavior: 'smooth',
-      })
-      setScrollPosition(scrollPosition - scrollOffset) // state 업데이트
-    }
-  }
+	const prevBtnHandler = () => {
+		const container = containerRef.current
+		if (container) {
+			const scrollOffset = 2000 // 클릭 당 스크롤 이동 범위
+			container.scrollTo({
+				// 컨테이너 스크롤
+				left: scrollPosition - scrollOffset, // 현재 위치 - 클릭 당 스크롤 이동 범위
+				behavior: 'smooth',
+			})
+			setScrollPosition(scrollPosition - scrollOffset) // state 업데이트
+		}
+	}
 
   const nextBtnHandler = () => {
-    const container = containerRef.current
-    if (container) {
-      const scrollOffset = 325
-      container.scrollTo({
-        left: scrollPosition + scrollOffset,
-        behavior: 'smooth',
-      })
-      setScrollPosition(scrollPosition + scrollOffset)
-    }
-  }
+      const container = containerRef.current
+      if (container) {
+        const scrollOffset = 2000
+        container.scrollTo({
+          left: scrollPosition + scrollOffset,
+          behavior: 'smooth',
+        })
+        setScrollPosition(scrollPosition + scrollOffset)
+        setShowPrevButton(true)
+      }
+	}
 
-  /*편집모드 전환*/
-  const [ editMode, setEditMode ] = useState(false)
+	/*편집모드 전환*/
+	const [editMode, setEditMode] = useState(false)
 
-  
+	return (
+		<div className={style['container']}>
+			{/* 사이드바 Start */}
+			<div className={style['sidebar']}>
+				<div className={style['sidebar-title']}>
+					<p className={style['trip-title']}>강릉 여행</p>
+					<p className={style['travel-period']}>2023.6.22 ~ 6.28</p>
+				</div>
+				<Scrollbars
+					style={{ height: '100%', width: '100%' }}
+					thumbSize={120}
+					autoHide
+					autoHideTimeout={1000}
+					autoHideDuration={1000}
+					renderTrackHorizontal={(props) => <div {...props} className={style['track-horizontal']} />}
+					renderThumbHorizontal={(props) => <div {...props} className={style['thumb-horizontal']} />}>
+					{/* Day1, 2, 3... 블록 */}
+					<div className={style['dayplan-wrapper']}>
+						{editMode
+							? //편집모드인 경우
+							  dateList.map((item, index) => (
+									<DayPlanEditMode
+										key={index + 1}
+										orderDay={index + 1}
+										date={item}
+										planList={planList}
+										planListHandler={planListHandler}
+										setEditMode={setEditMode}
+									/>
+							  ))
+							: //편집모드가 아닌 경우
+							  dateList.map((item, index) => (
+									<DayPlan
+										key={index + 1}
+										orderDay={index + 1}
+										date={item}
+										planList={planList}
+										planListHandler={planListHandler}
+										setEditMode={setEditMode}
+									/>
+							  ))}
+					</div>
+				</Scrollbars>
+			</div>
+			{/* 사이드바 End */}
 
-  return (
-    <div className={style[ 'container' ]}>
-      {/* 사이드바 Start */}
-      <div className={style[ 'sidebar' ]}>
-        <div className={style[ 'sidebar-title' ]}>
-          <p className={style[ 'trip-title' ]}>강릉 여행</p>
-          <p className={style[ 'travel-period' ]}>2023.6.22 ~ 6.28</p>
-        </div>
-        <Scrollbars
-          style={{ height: '100%', width: '100%' }}
-          thumbSize={120}
-          autoHide
-          autoHideTimeout={1000}
-          autoHideDuration={1000}
-          renderTrackHorizontal={(props) => <div {...props} className={style[ 'track-horizontal' ]} />}
-          renderThumbHorizontal={(props) => <div {...props} className={style[ 'thumb-horizontal' ]} />}>
-          {/* Day1, 2, 3... 블록 */}
-          <div className={style[ 'dayplan-wrapper' ]}>
-            {editMode
-              ? //편집모드인 경우
-              dateList.map((item, index) => (
-                <DayPlanEditMode
-                  key={index + 1}
-                  orderDay={index + 1}
-                  date={item}
-                  planList={planList}
-                  planListHandler={planListHandler}
-                  setEditMode={setEditMode}
-                />
-              ))
-              : //편집모드가 아닌 경우
-              dateList.map((item, index) => (
-                <DayPlan
-                  key={index + 1}
-                  orderDay={index + 1}
-                  date={item}
-                  planList={planList}
-                  planListHandler={planListHandler}
-                  setEditMode={setEditMode}
-                />
-              ))}
-          </div>
-        </Scrollbars>
-      </div>
-      {/* 사이드바 End */}
+			{/* 구글맵 Start */}
+			<div className={style['map']}>
+				{/* //window가 로드 된 시점에서 google map을 렌더링함. */}
+				{winReady ? <Map /> : null}
+			</div>
+			{/* 구글맵 End */}
 
-      {/* 구글맵 Start */}
-      <div className={style[ 'map' ]}>
-        {/* //window가 로드 된 시점에서 google map을 렌더링함. */}
-        {winReady ? <Map /> : null}
-      </div>
-      {/* 구글맵 End */}
+			{/* 선택한 장소 목록 Start */}
+			<div className={style['selected-place']}>
+				<div className={style['day-wrapper']}>
+					<button
+						className={style['day-button']}
+						style={{ color: 'white', backgroundColor: '#fc7300', border: 'solid 1px #fc7300' }}>
+						DAY1
+					</button>
+					<button className={style['day-button']}>DAY2</button>
+					<button className={style['day-button']}>DAY3</button>
+				</div>
 
-      {/* 선택한 장소 목록 Start */}
-      <div className={style[ 'selected-place' ]}>
-        <div className={style[ 'day-wrapper' ]}>
-          <button
-            className={style[ 'day-button' ]}
-            style={{ color: 'white', backgroundColor: '#fc7300', border: 'solid 1px #fc7300' }}>
-            DAY1
-          </button>
-          <button className={style[ 'day-button' ]}>DAY2</button>
-          <button className={style[ 'day-button' ]}>DAY3</button>
-        </div>
-
-        <div className={style[ "selected-item-wrapper" ]} ref={containerRef}>
-        {showPrevButton && (
-          <div
-            className={style[ "selected-item-prev-btn" ]}
-            onClick={prevBtnHandler}
-          >
-            왼쪽
-          </div>
-        )}
-          {Array.from(
-            { length: 7 },
-            (
-              _, index  /*item이 사용되지 않아, _ 표기*/
-            ) => (
-              <SelectedPlace key={index + 1} order={index + 1} />
-            )
-          )}
-        {showNextButton && (
-          <div className={style[ "slide-button next" ]} onClick={nextBtnHandler}>
-            오른쪽
-          </div>
-        )}
-        </div>
-      </div>
-      {/* 선택한 장소 목록 End */}
-    </div>
-  )
+				<div className={style['selected-item-container']}>
+					<div className={style['slide-prev-btn-wrapper']} onClick={prevBtnHandler}>
+						{showPrevButton && (
+							<img
+								src={process.env.PUBLIC_URL + '/images/icon_arrow_left_orange.png'}
+								alt='arrow_orange'
+								className={style['slide-btn']}
+							/>
+						)}
+					</div>
+					<div className={style['selected-item-wrapper']} ref={containerRef}>
+						{planList
+							.filter((item) => item.type !== 'memo')
+							.map((item, index) => (
+								<SelectedPlace key={index + 1} order={index + 1} planList={item} />
+							))}
+					</div>
+					{showNextButton && (
+						<div className={style['slide-next-btn-wrapper']} onClick={nextBtnHandler}>
+							<img
+								src={process.env.PUBLIC_URL + '/images/icon_arrow_right_orange.png'}
+								alt='arrow_orange'
+								className={style['slide-btn']}
+							/>
+						</div>
+					)}
+				</div>
+			</div>
+			{/* 선택한 장소 목록 End */}
+		</div>
+	)
 }
