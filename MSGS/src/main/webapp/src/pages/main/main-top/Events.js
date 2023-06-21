77e625253f8ea9fd9900831bf30b2eba3020d390
jihyeon <wisedew2@gmail.com>
2023-06-21 21:34:12 +0900

    fix : commit test

--- MSGS/src/main/webapp/src/pages/main/main-top/Events.js
+++ MSGS/src/main/webapp/src/pages/main/main-top/Events.js
@@ -3,23 +3,11 @@ import React, { useEffect, useReducer, useState } from "react";
 import styles from "./MainTop.module.css";
 import EventItem from "./EventItem";
 
-const Events = () => {
-    const items = [
-        { id: 1, text1: "한국의 알프스", text2: "하동 여행 코스 추천 🌳" },
-        {
-            id: 2,
-            text1: "여유로운 바다 강릉",
-            text2: "강릉 아르떼 뮤지엄 할인 🎵",
-        },
-        {
-            id: 3,
-            text1: "작은 대한민국",
-            text2: "영월, 봄에 가볼 곳 모음집 🌸",
-        },
-    ];
 
-<<<<<<< Updated upstream
-=======
+const Events = () => {
+    
+    const API_KEY = 'tubCNUm%2FYUF%2FD2wDWLTebna0yukLqBKsQTPu4iAlmY0F26uG428F0QRxe%2ByLehqGeulixiTmPSWWEO3V18Tuxg%3D%3D';
+
     const [data, setData] = useState(null); 
   
     async function getData() {
@@ -50,16 +38,20 @@ const Events = () => {
     
     
       
->>>>>>> Stashed changes
     return (
         <section className={styles["section-event"]}>
-            {items.map((eventData) => (
-                <EventItem
-                    key={eventData.id}
-                    text1={eventData.text1}
-                    text2={eventData.text2}
-                />
-            ))}
+            
+        {
+            filteredData.map((item, index) => index < 3 && (
+            <EventItem
+            key={index}
+            title={item.title}
+            firstimage={item.firstimage}
+            />
+        ))}
+
+               
+            
         </section>
     );
 };
