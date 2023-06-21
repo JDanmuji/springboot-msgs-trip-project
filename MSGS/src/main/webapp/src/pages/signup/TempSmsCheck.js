9c82cc72e73b2f74eac232f5f9c4b183c119473d
jihyelisa <jihyelisak@gmail.com>
2023-06-21 12:20:23 +0900

    fix : 회원가입 번호인증 파라미터 안 붙도록 수정

--- MSGS/src/main/webapp/src/pages/signup/TempSmsCheck.js
+++ MSGS/src/main/webapp/src/pages/signup/TempSmsCheck.js
@@ -1,30 +1,47 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const TempSmsCheck = () => {
     const [to, setTo] = useState("");
 
-    const buttonHandler = async () => {
+    const buttonHandler = async (event) => {
+        event.preventDefault();
+
         try {
-            console.log(to);
+            const response = await axios.post("/user/signup/smscheck", { to });
 
-            const response = await fetch("/user/signup/smscheck", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(to),
-            });
-
-            if (response.ok) {
-                console.log("문자 발송 성공");
+            if (response.data === "exist") {
+                console.log("Phone number already exists");
             } else {
-                console.log("문자 발송 실패");
+                console.log("Authentication number sent");
             }
         } catch (error) {
-            // 네트워크 오류 등 예외 처리
-            console.log("오류 발생", error);
+            console.log("Error occurred:", error);
         }
     };
+
+    // const buttonHandler = async () => {
+    // try {
+    //     console.log(to);
+
+    //     const response = await fetch("/user/signup/smscheck", {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //         },
+    //         body: JSON.stringify(to),
+    //     });
+
+    //     if (response.ok) {
+    //         console.log("문자 발송 성공");
+    //     } else {
+    //         console.log("문자 발송 실패");
+    //     }
+    // } catch (error) {
+    //     // 네트워크 오류 등 예외 처리
+    //     console.log("오류 발생", error);
+    // }
+    // };
 
     return (
         <form>
