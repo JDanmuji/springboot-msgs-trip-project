050d7985e61cb8aae64f1ebb7cb7bf7effad623c
eunyeoung22 <121652152+eunyeoung22@users.noreply.github.com>
2023-06-23 15:20:14 +0900

    feat : update index.html, app.js, env

--- MSGS/src/main/webapp/src/index.js
+++ MSGS/src/main/webapp/src/index.js
@@ -6,10 +6,12 @@ import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	// <React.StrictMode> ->react-beautiful-dnd 사용 위해 지움
-	<App />
-	// </React.StrictMode>
-)
+    // <React.StrictMode> ->react-beautiful-dnd 사용 위해 지움
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+        <App />
+    </GoogleOAuthProvider>
+    // </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
