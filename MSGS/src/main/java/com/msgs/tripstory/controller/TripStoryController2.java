b6eb734c35eb73a4557b0c1802d818426985efc1
jihyelisa <jihyelisak@gmail.com>
2023-06-25 23:47:08 +0900

    feat & fix : GoogleMapPolyline.js 컴포넌트 생성, story 상세페이지 점선 있는 지도로 변경

--- MSGS/src/main/java/com/msgs/tripstory/controller/TripStoryController2.java
+++ MSGS/src/main/java/com/msgs/tripstory/controller/TripStoryController2.java
@@ -22,91 +22,126 @@ public class TripStoryController2 {
 	
 	// 이야기 day1~2 임시데이터
 	private JSONObject getDummyData() {
-		JSONObject dummyData = new JSONObject();
+	    JSONObject dummyData = new JSONObject();
 
-		dummyData.put("tripId", 1);
-		dummyData.put("title", "즐거운 강릉 여행 다녀오기");
-		dummyData.put("comment", "나는 즐거운 강릉 여행을 다녀왔다. 리뷰 시작 고고");
-		dummyData.put("rating", 4);
+	    dummyData.put("tripId", 1);
+	    dummyData.put("title", "즐거운 강릉 여행 다녀오기");
+	    dummyData.put("comment", "나는 즐거운 강릉 여행을 다녀왔다. 리뷰 시작 고고");
+	    dummyData.put("rating", 4);
 
-		JSONArray dateList = new JSONArray();
-		dateList.put("2023-05-23");
-		dateList.put("2023-05-24");
-		dateList.put("2023-05-24");
-		dateList.put("2023-05-24");
-		dateList.put("2023-05-24");
-		dateList.put("2023-05-24");
-		dateList.put("2023-05-24");
-		dateList.put("2023-05-24");
-		dateList.put("2023-05-24"); // day btn 테스트용 데이터
-		dummyData.put("date_list", dateList);
+	    JSONArray dateList = new JSONArray();
+	    dateList.put("2023-05-23");
+	    dateList.put("2023-05-24");
+	    dateList.put("2023-05-24");
+	    dateList.put("2023-05-24");
+	    dateList.put("2023-05-24");
+	    dateList.put("2023-05-24");
+	    dateList.put("2023-05-24");
+	    dateList.put("2023-05-24");
+	    dateList.put("2023-05-24"); // day btn 테스트용 데이터
+	    dummyData.put("date_list", dateList);
 
-		dummyData.put("city", "강원북도 강릉시");
+	    dummyData.put("city", "강원북도 강릉시");
 
-		JSONArray tripDetailList = new JSONArray();
+	    JSONArray tripDetailList = new JSONArray();
 
-		JSONObject tripDetail1 = new JSONObject();
-		tripDetail1.put("day", "1_detail_day1");
-		tripDetail1.put("dayDate", "2023-05-23");
-		tripDetail1.put("dayCount", "1");
-		tripDetail1.put("content", "day1 콘텐트이묘! 여기서는 강릉여행 첫날에 뭘 했는지 알아보겠습니당");
+	    // day1
+	    JSONObject tripDetail1 = new JSONObject();
+	    tripDetail1.put("day", "1_detail_day1");
+	    tripDetail1.put("dayDate", "2023-05-23");
+	    tripDetail1.put("dayCount", "1");
+	    tripDetail1.put("content", "day1 콘텐트이묘! 여기서는 강릉여행 첫날에 뭘 했는지 알아보겠습니다.");
 
-		JSONArray tripDayDetail1 = new JSONArray();
+	    JSONArray tripDayDetail1 = new JSONArray();
+	    JSONArray tripDayDetail2 = new JSONArray();
 
-		// day1 장소1
-		JSONObject tripDayDetail1_1 = new JSONObject();
-		tripDayDetail1_1.put("tripDetailId", "1_Day1");
-		tripDayDetail1_1.put("title", "안목해변");
-		tripDayDetail1_1.put("subtitle", "관광명소");
-		tripDayDetail1_1.put("mapLon", "128.8784972");
-		tripDayDetail1_1.put("mapLat", "37.74913611");
-		tripDayDetail1_1.put("scheduleOrder", 1);
-		tripDayDetail1_1.put("rating", 4);
-		tripDayDetail1_1.put("content", "안목해변에서 신나는 하루 보냈음 !! 짱");
+	    // day1 장소1
+	    JSONObject tripDayDetail1_1 = new JSONObject();
+	    tripDayDetail1_1.put("tripDetailId", "1_Day1");
+	    tripDayDetail1_1.put("title", "안목해변");
+	    tripDayDetail1_1.put("subtitle", "관광명소");
+	    tripDayDetail1_1.put("mapLon", 128.8784972);
+	    tripDayDetail1_1.put("mapLat", 37.74913611);
+	    tripDayDetail1_1.put("scheduleOrder", 1);
+	    tripDayDetail1_1.put("rating", 4);
+	    tripDayDetail1_1.put("content", "안목해변에서 신나는 하루를 보냈어요!! 짱!");
 
-		JSONArray img1_1 = new JSONArray();
-		img1_1.put("https://images.pexels.com/photos/4945061/pexels-photo-4945061.jpeg");
-		img1_1.put("https://images.pexels.com/photos/6181092/pexels-photo-6181092.jpeg");
-		img1_1.put("https://pbs.twimg.com/media/EA9UJBjU4AAdkCm.jpg");
-		tripDayDetail1_1.put("img", img1_1);
-		
-		// day1 장소2
-		JSONObject tripDayDetail2_1 = new JSONObject();
-		tripDayDetail2_1.put("tripDetailId", "2_Day1");
-		tripDayDetail2_1.put("title", "속초 중앙 시장");
-		tripDayDetail2_1.put("subtitle", "관광명소");
-		tripDayDetail2_1.put("mapLon", 128.5727574);
-		tripDayDetail2_1.put("mapLat", 38.2075461);
-		tripDayDetail2_1.put("scheduleOrder", 2);
-		tripDayDetail2_1.put("rating", 3);
-		String content = "속초중앙시장은 정말 활기 넘치고 매력적인 장소입니다. 다양한 상점과 가게들이 모여있어서 쇼핑하기에도 좋아요. 특히 신선한 해산물과 지역 특산품을 구매할 수 있는 곳이라서 속초를 방문한 여행객들에게 추천합니다. 시장 안에서 느껴지는 분위기도 아주 좋고, 현지 음식도 맛있어서 여행 중에 한 번쯤 꼭 들러보세요. 속초중앙시장에서 재미있는 시간을 보낼 수 있어서 너무 기뻤고, 다시 방문하고 싶은 곳 중 하나입니다. 추억에 남는 여행을 원하신다면 속초중앙시장을 놓치지 마세요! - gpt가 써준 페이크리뷰";
-		tripDayDetail2_1.put("content", content);
+	    JSONArray img1_1 = new JSONArray();
+	    img1_1.put("https://images.pexels.com/photos/4945061/pexels-photo-4945061.jpeg");
+	    img1_1.put("https://images.pexels.com/photos/6181092/pexels-photo-6181092.jpeg");
+	    img1_1.put("https://pbs.twimg.com/media/EA9UJBjU4AAdkCm.jpg");
+	    tripDayDetail1_1.put("img", img1_1);
 
-		JSONArray img2_1 = new JSONArray();
-		img2_1.put("https://images.pexels.com/photos/1234567/pexels-photo-1234567.jpeg");
-		img2_1.put("https://images.pexels.com/photos/14894654/pexels-photo-14894654.jpeg");
-		tripDayDetail2_1.put("img", img2_1);
+	    // day1 장소2
+	    JSONObject tripDayDetail1_2 = new JSONObject();
+	    tripDayDetail1_2.put("tripDetailId", "1_Day2");
+	    tripDayDetail1_2.put("title", "속초 중앙 시장");
+	    tripDayDetail1_2.put("subtitle", "관광명소");
+	    tripDayDetail1_2.put("mapLon", 128.5727574);
+	    tripDayDetail1_2.put("mapLat", 38.2075461);
+	    tripDayDetail1_2.put("scheduleOrder", 2);
+	    tripDayDetail1_2.put("rating", 3);
+	    String content = "속초 중앙 시장은 정말 활기 넘치고 매력적인 장소입니다. 다양한 상점과 가게들이 모여있어서 쇼핑하기에도 좋아요. 특히 신선한 해산물과 지역 특산품을 구매할 수 있는 곳이라서 속초를 방문한 여행객들에게 추천합니다. 시장 안에서 느껴지는 분위기도 아주 좋고, 현지 음식도 맛있어서 여행 중에 한 번쯤 꼭 들러보세요. 속초 중앙 시장에서 재미있는 시간을 보낼 수 있어서 너무 기뻤고, 다시 방문하고 싶은 곳 중 하나입니다. 추억에 남는 여행을 원하신다면 속초 중앙 시장을 놓치지 마세요! - gpt가 써준 페이크 리뷰";
+	    tripDayDetail1_2.put("content", content);
 
-		tripDayDetail1.put(tripDayDetail1_1);
-		tripDayDetail1.put(tripDayDetail2_1);
+	    JSONArray img1_2 = new JSONArray();
+	    img1_2.put("https://images.pexels.com/photos/1234567/pexels-photo-1234567.jpeg");
+	    img1_2.put("https://images.pexels.com/photos/14894654/pexels-photo-14894654.jpeg");
+	    tripDayDetail1_2.put("img", img1_2);
 
-		tripDetail1.put("tripDayDetail", tripDayDetail1);
+	    tripDayDetail1.put(tripDayDetail1_1);
+	    tripDayDetail1.put(tripDayDetail1_2);
 
-		// day2, 장소 데이터 없음
-		JSONObject tripDetail2 = new JSONObject();
-		tripDetail2.put("day", "1_detail_day2");
-		tripDetail2.put("dayDate", "2023-05-24");
-		tripDetail2.put("dayCount", "2");
-		tripDetail2.put("content", "day2 콘텐트");
+	    tripDetail1.put("tripDayDetail", tripDayDetail1);
 
-//		JSONArray tripDayDetail2 = new JSONArray();
-//		tripDetail1.put("tripDayDetail", tripDayDetail2);
+	    // day2 장소1
+	    JSONObject tripDetail2 = new JSONObject();
+	    tripDetail2.put("day", "1_detail_day2");
+	    tripDetail2.put("dayDate", "2023-05-24");
+	    tripDetail2.put("dayCount", "2");
+	    tripDetail2.put("content", "day2 콘텐트");
 
-		tripDetailList.put(tripDetail1);
-		tripDetailList.put(tripDetail2);
-		dummyData.put("tripDetailList", tripDetailList);
+	    // day2
+	    JSONObject tripDayDetail2_1 = new JSONObject();
+	    tripDayDetail2_1.put("tripDetailId", "2_Day1");
+	    tripDayDetail2_1.put("title", "설악산");
+	    tripDayDetail2_1.put("subtitle", "자연명소");
+	    tripDayDetail2_1.put("mapLon", 128.4850524);
+	    tripDayDetail2_1.put("mapLat", 38.0917697);
+	    tripDayDetail2_1.put("scheduleOrder", 1);
+	    tripDayDetail2_1.put("rating", 5);
+	    tripDayDetail2_1.put("content", "설악산은 아름다운 자연 경관으로 유명한 산으로, 등반객뿐만 아니라 관광객들에게도 인기가 많습니다. 해발 1,708m의 대표적인 봉우리인 동대천봉과 남대천봉은 특히 유명하며, 등반을 통해 멋진 전망을 즐길 수 있습니다. 또한, 설악산에는 다양한 산책로와 자연 탐방로도 마련되어 있어 자연과 함께 산책을 즐기기에도 좋습니다. 설악산은 아름다운 풍경과 신선한 공기를 마시며 자연과 깊은 소통을 할 수 있는 멋진 장소입니다.");
 
-		return dummyData;
+	    JSONArray img2_1 = new JSONArray();
+	    img2_1.put("https://images.pexels.com/photos/1571192/pexels-photo-1571192.jpeg");
+	    img2_1.put("https://images.pexels.com/photos/987654/pexels-photo-987654.jpeg");
+	    tripDayDetail2_1.put("img", img2_1);
+
+	    // day2 장소2
+	    JSONObject tripDayDetail2_2 = new JSONObject();
+	    tripDayDetail2_2.put("tripDetailId", "2_Day2");
+	    tripDayDetail2_2.put("title", "영금정");
+	    tripDayDetail2_2.put("subtitle", "자연명소");
+	    tripDayDetail2_2.put("mapLon", 128.4766053);
+	    tripDayDetail2_2.put("mapLat", 38.1622684);
+	    tripDayDetail2_2.put("scheduleOrder", 2);
+	    tripDayDetail2_2.put("rating", 4);
+	    tripDayDetail2_2.put("content", "영금정은 설악산 내에 위치한 아름다운 계곡입니다. 푸른 물과 아름다운 바위들이 어우러져 자연 그대로의 아름다움을 선사합니다. 여름철에는 시원한 물놀이를 즐길 수 있으며, 가을에는 단풍 구경이 아름다운 명소입니다. 영금정은 자연 속에서 힐링과 즐거움을 동시에 느낄 수 있는 특별한 장소입니다.");
+
+	    JSONArray img2_2 = new JSONArray();
+	    img2_2.put("https://images.pexels.com/photos/1571193/pexels-photo-1571193.jpeg");
+	    tripDayDetail2_2.put("img", img2_2);
+
+	    tripDayDetail2.put(tripDayDetail2_1);
+	    tripDayDetail2.put(tripDayDetail2_2);
+
+	    tripDetail2.put("tripDayDetail", tripDayDetail2);
+
+	    tripDetailList.put(tripDetail1);
+	    tripDetailList.put(tripDetail2);
+	    dummyData.put("tripDetailList", tripDetailList);
+
+	    return dummyData;
 	}
 	
 	
@@ -127,7 +162,7 @@ public class TripStoryController2 {
 	}
 
 	@PostMapping("/getStoryLike")
-	public boolean getStoryLike(@RequestBody String data) {
+	public ResponseEntity<String> getStoryLike(@RequestBody String data) {
 		
 		// userId, storyId 데이터 받아오기
 		JSONObject requestData = new JSONObject(data);
@@ -136,9 +171,17 @@ public class TripStoryController2 {
 	    System.out.println(userId);
 	    System.out.println(storyId);
 		
-		// db 조인 데이터 받아오기
+	    // DB에서 가져올 데이터
+		// 해당 userId가 좋아요 눌렀는지 여부
+	    // 해당 storyId의 누적 좋아요 수
+
+	    // 임시데이터
+		JSONObject responseObj = new JSONObject();
+		responseObj.put("isLiked", true);
+		responseObj.put("likeCnt", 32);
 		
-		return true;
+		String responseStr = responseObj.toString();
+		return ResponseEntity.status(HttpStatus.OK).body(responseStr);
 	}
 
 	@PostMapping("/storyLikeUpdate")
--- MSGS/src/main/webapp/src/pages/tripstory/tripstory-detail/TripStoryDay.js
+++ MSGS/src/main/webapp/src/pages/tripstory/tripstory-detail/TripStoryDay.js
@@ -3,27 +3,35 @@ import React from "react";
 import styles from "./TripStoryDay.module.css";
 
 import TripStoryDayPlace from "./TripStoryDayPlace";
-import LocGoogleMap from "../../tripplace/LocGoogleMap";
+import GoogleMapPolyline from "../../../components/tripstory/tripstory-details/GoogleMapPolyline";
 
 const TripStoryDay = (props) => {
     const dayData = props.dayData;
+
+    // 지도에 표기할 좌표 데이터 추출
+    const mapDataList = [];
+    dayData.tripDayDetail.forEach((item, index) => {
+        const placeData = {
+            order: index + 1,
+            type: "place",
+            center: { lat: item.mapLat, lng: item.mapLon },
+        };
+        mapDataList.push(placeData);
+    });
 
     return (
         <div>
             <h2 className={styles["day-number"]}>
                 Day {dayData.dayCount} | {dayData.dayDate}
             </h2>
-            <p className={styles["day-content"]}>{dayData.content}</p>
+            {dayData.content && (
+                <p className={styles["day-content"]}>{dayData.content}</p>
+            )}
 
             {dayData.tripDayDetail && (
                 <>
                     {/* 각 day별 경로 표시된 구글맵 */}
-                    <LocGoogleMap // 경로 표시되는 걸로 새 컴포넌트 만들어야 함
-                        center={{
-                            lat: parseFloat(dayData.tripDayDetail[0].mapLat),
-                            lng: parseFloat(dayData.tripDayDetail[0].mapLon),
-                        }}
-                    />
+                    <GoogleMapPolyline mapDataList={mapDataList} />
 
                     {/* Day1에 간 장소 리스트 map 돌림 */}
                     <ul className={styles["day-detail-list"]}>
