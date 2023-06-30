b4214699c34c39ff50c7a8a2bd25811361c0780d
6dreamhigh <6dreamhigh@naver.com>
2023-06-30 11:04:29 +0900

    feat:mypage back

--- MSGS/src/main/webapp/src/pages/flight/flight-data/AirportData.js
+++ MSGS/src/main/webapp/src/pages/flight/flight-data/AirportData.js
@@ -1,14 +1,14 @@
 const Airport = [
-  { id: 1, location: "서울", eng: "NAARKS", kor: "인천, 김포국제공항" },
-  { id: 2, location: "제주", eng: "NAARKPC", kor: "제주국제공항" },
-  { id: 3, location: "부산", eng: "NAARKPK", kor: "김해국제공항" },
-  { id: 4, location: "김포", eng: "NAARKSS", kor: "김포국제공항" },
-  { id: 5, location: "인천", eng: "NAARKSI", kor: "인천국제공항" },
-  { id: 6, location: "광주", eng: "NAARKJJ", kor: "광주공항" },
-  { id: 7, location: "무안", eng: "NAARKJB", kor: "무안국제공항" },
-  { id: 8, location: "군산", eng: "KNAARKJK", kor: "군산공항" },
-  { id: 9, location: "대구", eng: "NAARKTN", kor: "대구국제공항" },
-  { id: 10, location: "사천", eng: "NAARKPS", kor: "사천공항" },
+  { id: 1, location: "서울", eng: "NAARKS", kor: "인천, 김포국제공항", regionId: "SEL" },
+  { id: 2, location: "제주", eng: "NAARKPC", kor: "제주국제공항", regionId: "CJU"  },
+  { id: 3, location: "부산", eng: "NAARKPK", kor: "김해국제공항", regionId: "PUS"  },
+  { id: 4, location: "김포", eng: "NAARKSS", kor: "김포국제공항", regionId: "GMP"  },
+  { id: 5, location: "인천", eng: "NAARKSI", kor: "인천국제공항", regionId: "ICN"  },
+  { id: 6, location: "광주", eng: "NAARKJJ", kor: "광주공항", regionId: "KWJ"  },
+  { id: 7, location: "무안", eng: "NAARKJB", kor: "무안국제공항", regionId: "MWX"  },
+  { id: 8, location: "군산", eng: "KNAARKJK", kor: "군산공항", regionId: "KUV"  },
+  { id: 9, location: "대구", eng: "NAARKTN", kor: "대구국제공항", regionId: "TAE"  },
+  { id: 10, location: "사천", eng: "NAARKPS", kor: "사천공항", regionId: "HIN"  },
 ];
 
 export default Airport;
