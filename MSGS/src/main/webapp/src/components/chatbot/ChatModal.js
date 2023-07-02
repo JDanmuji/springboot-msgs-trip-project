9e82d69e9d47bdac995a23e53f21a5e34f04586b
jihyelisa <jihyelisak@gmail.com>
2023-07-03 05:49:59 +0900

    feat : 챗봇 오픈 버튼 메인에 추가

--- MSGS/src/main/webapp/src/components/chatbot/ChatModal.js
+++ MSGS/src/main/webapp/src/components/chatbot/ChatModal.js
@@ -1,40 +1,42 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 
 import Chatbot from "react-chatbot-kit";
 import "react-chatbot-kit/build/main.css";
 
-import config from './bot/config.js';
-import MessageParser from './bot/MessageParser.js';
-import ActionProvider from './bot/ActionProvider.js';
+import config from "./bot/config.js";
+import MessageParser from "./bot/MessageParser.js";
+import ActionProvider from "./bot/ActionProvider.js";
 
 import "remixicon/fonts/remixicon.css";
 import "./ChatModal.css";
-import {Stomp} from "@stomp/stompjs";
+import { Stomp } from "@stomp/stompjs";
 import SockJS from "sockjs-client";
-
 
 const ChatModal = () => {
     const [isChatBot, setIsChatBot] = useState(false);
 
     const openChatConnect = () => {
         setIsChatBot(!isChatBot);
-    }
+    };
 
     return (
-        <>
+        <div>
             <div className="chat-bot-img-wrap" onClick={openChatConnect}>
-                <img src={process.env.PUBLIC_URL + "/images/chat_bot_icon.png"}  alt=""
-                className="chat-bot-img"/>
+                <img
+                    src={process.env.PUBLIC_URL + "/images/chat_bot_icon.png"}
+                    alt=""
+                    className="chat-bot-img"
+                />
             </div>
 
-            { isChatBot &&
-            <Chatbot
-                config={config}
-                messageParser={MessageParser}
-                actionProvider={ActionProvider}
-            />
-            }
-        </>
+            {isChatBot && (
+                <Chatbot
+                    config={config}
+                    messageParser={MessageParser}
+                    actionProvider={ActionProvider}
+                />
+            )}
+        </div>
     );
 };
 
