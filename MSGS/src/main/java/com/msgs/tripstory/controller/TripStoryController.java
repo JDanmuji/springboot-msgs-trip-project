df7b102626bce587c9b2abf10c84ed1e12f5840b
JDanmuji <Danmuji0724@gmail.com>
2023-07-02 11:59:32 +0900

    fix : imgae object story upload

--- MSGS/src/main/java/com/msgs/tripstory/controller/TripStoryController.java
+++ MSGS/src/main/java/com/msgs/tripstory/controller/TripStoryController.java
@@ -1,5 +1,15 @@
 package com.msgs.tripstory.controller;
 
+import com.amazonaws.SdkClientException;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.util.Base64;
 import com.msgs.msgs.dto.StoryBlockDTO;
 import com.msgs.msgs.dto.StoryRequestDTO;
@@ -7,6 +17,7 @@ import java.util.Map;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,11 +26,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.msgs.msgs.dto.TripStoryMainDTO;
 import com.msgs.msgs.entity.tripstory.StoryComment;
 import com.msgs.tripstory.service.TripStoryService;
 
+import jakarta.servlet.http.HttpSession;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 import java.util.List;
@@ -62,17 +80,36 @@ public class TripStoryController {
 
 	//프론트에서 받은 여행기 데이터를 DB에 저장함
 	@PostMapping("/info")
-	public ResponseEntity<Void> saveStory(@RequestBody StoryRequestDTO storyRequest){
+	public ResponseEntity<Void> saveStory(@RequestBody StoryRequestDTO storyRequest, HttpSession httpSess) throws Exception{
 		
 		
 		List<Object> imgList = (List<Object>) storyRequest.getStoryData().get("img");
 		
 		System.out.println("test+===============================================");
 		System.out.println(storyRequest.getStoryData());
+		
+		
 		System.out.println("test+===============================================");
 		System.out.println(imgList.get(0));
 		
 		
+		String imgData = imgList.get(0).toString();
+		byte[] byteArr = Base64.decode(imgData.substring(imgData.indexOf(",") + 1));
+		
+		
+		
+
+
+			    		
+		System.out.println("+++++++++++++++++++++DECODE");
+		System.out.println(byteArr);
+		
+		uploadFilesSample(byteArr, httpSess);
+		
+//		File file = new File("path/to/save/image.jpg");
+//		FileOutputStream os = new FileOutputStream(file);
+//		os.write(data);
+//		os.close();
 	   // byte[] decodedBytes = Base64.decode(storyRequest.getStoryData().get("img"));
 		
 //		System.out.println(storyRequest.getStoryData());
@@ -126,6 +163,74 @@ public class TripStoryController {
 	public List<TripStoryMainDTO> getStoryList(){
 		System.out.println("=====Controller=====");
 		return tripStoryService.getStoryList();
+	}
+	
+	
+	public String uploadFilesSample(byte[] file, HttpSession session) throws Exception{
+
+		String bucketName = "msgs-file-server";
+		String path = "/user-image";
+		String originalName;
+		long size;
+
+	
+
+		String endPoint = "https://kr.object.ncloudstorage.com";
+		String regionName = "kr-standard";
+		String accessKey = "6fCMolib7QBe1JKwSafq";
+		String secretKey = "miJ3BdZsKPsE3WLliwHPJbJS7qaxby6F6rDiVTJa";
+
+		// S3 client
+		AmazonS3 s3 = AmazonS3ClientBuilder.standard()
+				.withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endPoint, regionName))
+				.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+				.build();
+
+		try {
+
+			List<String> imagePathList = new ArrayList<>();
+//
+//			for (MultipartFile multipartFile: list) {
+//			
+//				String imagePath = s3.getUrl(bucketName + path, originalName).toString(); // 접근가능한 URL 가져오기
+//				imagePathList.add(imagePath);
+//				
+//				
+//			
+//				
+//			}
+			
+			ByteArrayInputStream fileData = new ByteArrayInputStream(file);
+//			originalName = ((MultipartFile) fileData).getOriginalFilename();
+//			size = ((MultipartFile) fileData).getSize();
+
+			
+			ObjectMetadata metadata = new ObjectMetadata();
+			metadata.setContentLength(file.length);
+			metadata.setContentType("image/png");
+			metadata.setCacheControl("public, max-age=31536000");
+			
+			
+			// 업로드
+			s3.putObject(
+					new PutObjectRequest(bucketName + path, "test3", fileData, metadata)
+							.withCannedAcl(CannedAccessControlList.PublicRead)
+			);
+
+
+			System.out.println(imagePathList);
+
+		} catch (AmazonS3Exception e) {
+			e.printStackTrace();
+		} catch(SdkClientException e) {
+			e.printStackTrace();
+		}
+		
+	
+
+
+		return  "success";
+
 	}
 
 
